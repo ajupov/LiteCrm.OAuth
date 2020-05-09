e638d89573fc9e53f43d67ee4e2b7e37125eeb5a
Usman Ayupov <u.ayupov@dodopizza.com>
2020-05-09 10:50:49 +0300

    use https

--- Options/LiteCrmOAuthDefaults.cs
+++ Options/LiteCrmOAuthDefaults.cs
@@ -4,8 +4,8 @@
     {
         public const string OpenIdScope = "openid";
         public const string ProfileScope = "profile";
-        public const string AuthorizationUrl = "http://identity.litecrm.org/oauth/authorize";
-        public const string UserInfoUrl = "http://identity.litecrm.org/oauth/userinfo";
-        public const string TokenUrl = "http://identity.litecrm.org/oauth/token";
+        public const string AuthorizationUrl = "https://identity.litecrm.org/oauth/authorize";
+        public const string UserInfoUrl = "https://identity.litecrm.org/oauth/userinfo";
+        public const string TokenUrl = "https://identity.litecrm.org/oauth/token";
     }
 }
